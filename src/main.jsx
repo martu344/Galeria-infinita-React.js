323f2906f09de5d2de41adc14384945f3d7fedf5
martin bottaro <martinbottaro34@gmail.com>
2023-07-10 14:03:51 -0300

    dsd

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider,CSSReset, extendTheme } from '@chakra-ui/react'
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg:'#242424'
+        
+      }
+    }
+  }
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-     <ChakraProvider bg='#3f4140'/>
+    <ChakraProvider theme={theme}>
+    <CSSReset />
      <App />
-    <ChakraProvider/>
-    
+    </ChakraProvider>
   </React.StrictMode>,
 )
